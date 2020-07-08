65dfc9c8bb48448594f6342522c3d555a972615e
Renato Moutinho Silva <renato.moutinho@gmail.com>
2020-07-08 10:05:33 -0300

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero.model';
+
+export const HEROES:Hero[] = [
+  {id: 11, name: "Dr Estranho"},
+  {id: 12, name: "Homem-aranha"},
+  {id: 13, name: "Super-homem"},
+  {id: 14, name: "Ciclope"},
+  {id: 15, name: "Wolverine"},
+  {id: 16, name: "Feiticeira Escarlate"}
+]
